b7c6d0c560110dbf889efdb81d9634bce55e1bb1
WraithOne <wraithone@outlook.de>
2015-12-27 12:04:38 +0100

    Initial Commit

---
+++ ZFX_Action_IX/Includes.h
@@ -0,0 +1,16 @@
+#pragma once
+////////////////////////////////////////////////////////////
+///
+/// ZFX Action IX
+/// Theme: Digging / Mob
+///
+/// Sunday, 2015-12-27 19:00 til Wendsday 2015-12-30, 19:00
+///
+/// Entry by WraithOne
+///
+////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
---
+++ ZFX_Action_IX/Main.cpp
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////
+///
+/// ZFX Action IX
+/// Theme: Digging / Mob
+///
+/// Sunday, 2015-12-27 19:00 til Wendsday 2015-12-30, 19:00
+///
+/// Entry by WraithOne
+///
+////////////////////////////////////////////////////////////
+
+// Includes
+#include "Includes.h"
+
+int main(int argc, char* args[])
+{
+	//Start SDL
+	SDL_Init( SDL_INIT_EVERYTHING );
+
+	//Quit SDL 
+	SDL_Quit();
+	return 0;
+}
