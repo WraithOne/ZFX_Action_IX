4c287e841263d2685896433b9a7ed7f72a458fc5
WraithOne <wraithone@outlook.de>
2016-01-09 12:34:37 +0100

    Last Checkin

--- ZFX_Action_IX/Main.cpp
+++ ZFX_Action_IX/Main.cpp
@@ -12,10 +12,26 @@
 // Includes
 #include "Includes.h"
 
+SDL_Window* sdl_Window;
+SDL_Renderer* sdl_Renderer;
+SDL_Event sdl_Event;
+
 int main(int argc, char* args[])
 {
 	//Start SDL
 	SDL_Init( SDL_INIT_EVERYTHING );
+
+	// Create Window and Renderer
+	SDL_CreateWindowAndRenderer(800, 600, 0, &sdl_Window, &sdl_Renderer);
+
+	while(1)
+	{
+		SDL_PollEvent(&sdl_Event);
+		if (sdl_Event.type == SDL_QUIT)
+		{
+			break;
+		}
+	}
 
 	//Quit SDL 
 	SDL_Quit();
